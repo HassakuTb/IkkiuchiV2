3dc2f0780102af38b6e9142b4d93f5303c28cb78
HassakuTb <hassaku.sc@gmail.com>
2018-07-08 23:47:23 +0900

    lobby button interactable control

---
+++ Assets/Scripts/LobbyScenes/Controllers/LobbySceneController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Ikkiuchi.LobbyScenes.Models;
+
+namespace Ikkiuchi.LobbyScenes.Controllers {
+
+    public class LobbySceneController : MonoBehaviour{
+
+        public LobbySceneModel model;
+
+        public void ChangeRoomName(string newName) {
+            model.SetRoomName(newName);
+        }
+    }
+}
--- Assets/Scripts/Networkings/Networking.cs
+++ Assets/Scripts/Networkings/Networking.cs
@@ -1,17 +1,37 @@
 ﻿using UnityEngine;
+using UniRx;
 
 namespace Ikkiuchi.Networkings {
     public class Networking : MonoBehaviour{
 
         IPhotonNetworkWrapper photon = new PhotonNetworkWrapper();
+        
+        private IReactiveProperty<bool> isConnected = new ReactiveProperty<bool>(false);
+
+        /// <summary>
+        /// ロビーに接続できているか
+        /// </summary>
+        public IReadOnlyReactiveProperty<bool> IsConnected {
+            get {
+                return isConnected.ToReadOnlyReactiveProperty();
+            }
+        }
 
         private void Start() {
             DontDestroyOnLoad(gameObject);
             photon.ConnectUsingSettings();  //  photonに接続
         }
 
+        //  Auto接続なのでphotonに繋いだらLobbyに即時はいる
         private void OnJoinedLobby() {
-            Debug.Log("joined to lobby");
+            Debug.Log("OnJoinedLobby");
+            isConnected.Value = true;
+        }
+
+        //  photonとの接続が切れたとき
+        private void OnDisconnectedFromServer(NetworkDisconnection info) {
+            Debug.Log("OnDisconnectedFromServer");
+            isConnected.Value = false;
         }
     }
 }
