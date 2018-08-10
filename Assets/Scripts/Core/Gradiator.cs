aa25197464ae6ac637cc787270c7faa3ff5517a4
HassakuTb <hassaku.sc@gmail.com>
2018-08-10 19:47:32 +0900

    spike

--- Assets/Scripts/Core/Gradiator.cs
+++ Assets/Scripts/Core/Gradiator.cs
@@ -8,16 +8,44 @@ namespace IkkiuchiV2.Core {
     public interface IGradiator {
 
         //  絶対位置
-        Pos Positon { get; }
-
-        //  向き
-        GradiatorDirection Direction { get; }
+        Pos Position { get; }
 
         //  相対位置→絶対位置変換
         Pos RelativePosToAbsolute(RelativePos relative);
-        
-        //  持ち主のプレイヤー
-        IPlayer Owner { get; }
+    }
+
+    //  実装
+    public class Gradiator : IGradiator {
+
+        //  絶対位置
+        public Pos Position { get; private set; }
+
+        //  向き(consistant)
+        private GradiatorDirection Direction { get; set; } 
+
+        /// <summary>
+        /// インスタンス初期化
+        /// </summary>
+        /// <param name="initPos">初期位置</param>
+        /// <param name="direction">駒の向き Player1ならNorth, Playwe2ならSouth</param>
+        public Gradiator(Pos initPos, GradiatorDirection direction, IPlayer owner) {
+            Position = initPos;
+            Direction = direction;
+        }
+
+        //  相対位置→絶対位置変換
+        public Pos RelativePosToAbsolute(RelativePos relative) {
+            //  南向きプレイヤー(P2)は相対位置を逆転させて加算する
+            switch (Direction) {
+                case GradiatorDirection.North:
+                    return new Pos(Position.X + relative.X, Position.Y + relative.Y);
+                case GradiatorDirection.South:
+                    return new Pos(Position.X - relative.X, Position.Y - relative.Y);
+                default:
+                    UnityEngine.Assertions.Assert.IsTrue(false);
+                    return Position;
+            }
+        }
     }
 
     //  IEnumerable<RelativePos>拡張
---
+++ Assets/Scripts/Core/SequenceControl.cs
@@ -0,0 +1,37 @@
+﻿using Zenject;
+
+namespace IkkiuchiV2.Core {
+        //  フェーズ
+    public enum Phase {
+        Start,  //  開始フェーズ
+        TurnStart,  //  ターン開始
+        MovePlot,   //  移動プロット
+        ActionPlot, //  行動プロット
+        Resolve,    //  解決
+        TurnEnd,    //  ターン終了
+        Settle, //  決着
+    }
+
+    //  DIインストーラ
+    class SequenceControlInstaller : Installer<SequenceControlInstaller> {
+        public override void InstallBindings() {
+            Container.Bind<>
+        }
+    }
+
+    //  シーケンスコントロール
+    public class SequenceControl {
+
+        //  プレイヤー1
+        [Inject]
+        private IPlayer player1;
+
+        //  プレイヤー2
+        [Inject]
+        private IPlayer player2;
+
+
+        
+
+    }
+}
