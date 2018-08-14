d615fdfd6d0f9d70cee59c869a109d48feff36c4
HassakuTb <hassaku.sc@gmail.com>
2018-08-14 18:32:50 +0900

    spike

---
+++ Assets/Scripts/Core/CardSet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace IkkiuchiV2.Core {
+    public interface ICardSet {
+
+        //  カードID → カード参照の取得
+        ICard GetCard(int cardId);
+
+    }
+
+    public class CardSet : ICardSet{
+
+        //  ランダムアクセス可能なデータ構造を使う
+        private List<ICard> cards = new List<ICard>(); 
+
+        public void CreateCardSet() {
+
+        }
+
+        public ICard GetCard(int cardId) {
+            return cards[cardId];
+        }
+    }
+}
